974579bd9d68c9cf79a9347ceb85130ac0001db2
Zeran Lu <zeranlu0326@gmail.com>
2025-09-26 13:10:35 -0230

    Updated Video Functionality
    In this commit, basic functionality was added to the video section. A sample video and sample poster was also added in the respective video and images folder.
    
    Basic video functions added: toggle play/pause button, replay button, volume control slider, fullscreen detection, and show/hide video player control panel.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu Functionality
+
 const hamButton = document.querySelector('.hamburger');
 const menuList = document.querySelector('#hamburger-list');
 
@@ -6,3 +8,73 @@ function toggleMenu() {
 }
 
 hamButton.addEventListener('click', toggleMenu);
+
+// Video Player Functionality
+
+const playerCon = document.querySelector(".player-container");
+const player = document.querySelector("video");
+const videoControls = document.querySelector(".video-controls");
+const toggleButton = document.querySelector("#toggle-button");
+    const playIcon = document.querySelector(".fa-play-circle-o");
+    const pauseIcon = document.querySelector(".fa-pause-circle-o");
+const replayButton = document.querySelector("#replay-button");
+const volumeSlider = document.querySelector("#change-vol");
+const fullScreen = document.querySelector("#full-screen");
+
+player.controls = false; 
+videoControls.classList.remove('hidden');
+
+function togglePlay () {
+    console.log("Playing video");
+    if (player.paused) {
+        player.play();
+        playIcon.classList.add('hidden');
+        pauseIcon.classList.remove('hidden');
+    } else {
+        console.log("Pausing video");
+        player.pause();
+        playIcon.classList.remove('hidden');
+        pauseIcon.classList.add('hidden');
+    }
+}
+
+function replayVideo () {
+    console.log("Replaying video");
+    player.pause();
+    player.currentTime = 0;
+}
+
+function changeVolume() {
+    console.log("Volume is:", volumeSlider.value);
+    player.volume = volumeSlider.value;
+}
+
+function toggleFullScreen() {
+    console.log("Exiting fullscren");
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        console.log("Toggling fullscreen");
+        playerCon.requestFullscreen();
+    }
+}
+
+function hideControls() {
+    console.log("Hiding controls");
+    videoControls.classList.add('hide');
+}
+
+function showControls() {
+    console.log("Showing controls");
+    videoControls.classList.remove('hide');
+}
+
+toggleButton.addEventListener("click", togglePlay);
+replayButton.addEventListener("click", replayVideo);
+volumeSlider.addEventListener("input", changeVolume);
+fullScreen.addEventListener("click", toggleFullScreen);
+
+videoControls.addEventListener("mouseenter", showControls);
+videoControls.addEventListener("mouseleave", hideControls);
+player.addEventListener("mouseenter", showControls);
+player.addEventListener("mouseleave", hideControls);
